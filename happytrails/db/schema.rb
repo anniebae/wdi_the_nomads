0e7b281971b11a87649b9a5cb8c90788afb42b64
dahvvv <davidjosephkoenig@gmail.com>
2014-11-19 15:41:00 -0500

    rename trails to trailsolutions

--- happytrails/db/schema.rb
+++ happytrails/db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119202644) do
+ActiveRecord::Schema.define(version: 20141119203817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "trails", force: true do |t|
+  create_table "trailsolutions", force: true do |t|
     t.integer  "prop_ID"
     t.string   "name"
     t.string   "location"
