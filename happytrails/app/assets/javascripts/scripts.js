bcf3d6172c29d6db4804afa3bfdf7cb608043783
dahvvv <davidjosephkoenig@gmail.com>
2014-11-20 10:49:52 -0500

    submit button makes hike jump to top

--- happytrails/app/assets/javascripts/scripts.js
+++ happytrails/app/assets/javascripts/scripts.js
@@ -6,7 +6,16 @@ $(document).ready(function() {
   });
 
   $('.welcome-submit').on('click', function(e){
+    e.preventDefault();
+    debugger;
     alert("u dun submitted! lol");
+    $('.hike-sign').animate({
+      "margin-top": "50px"
+    }, 10000, function(){
+      alert('u dunn');
+    });
+    
+    // $('.hike-sign').find($('h1')).animate({"font-size":"40px"});
   });
 
 });
