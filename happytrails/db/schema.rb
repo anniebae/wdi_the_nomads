4b9fcd30d6755f27c5f830d6d3fc2c1190520580
ianschmertzler <ianschmertzler@mac.com>
2014-11-20 10:27:30 -0500

    Set up trail solutions model pending form update

--- happytrails/db/schema.rb
+++ happytrails/db/schema.rb
@@ -11,8 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20141119203817) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "trailsolutions", force: true do |t|
     t.integer  "prop_ID"
