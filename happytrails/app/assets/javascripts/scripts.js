b81d6dfd1f85149d21d8656f2742f69f327e1a4f
dahvvv <davidjosephkoenig@gmail.com>
2014-11-20 10:22:33 -0500

    add event listener on welcome-submit

--- happytrails/app/assets/javascripts/scripts.js
+++ happytrails/app/assets/javascripts/scripts.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-    $(".drop-hike").hide();
-
+  $(".drop-hike").hide();
 
   $(".about").hover(function() { 
-
     $(".drop-hike").slideToggle();
+  });
+
+  $('.welcome-submit').on('click', function(e){
+    alert("u dun submitted! lol");
   });
 
 });
