83fb1e2708d5006bf4abf26bd1e053495a0ef875
ianschmertzler <ianschmertzler@mac.com>
2014-11-20 13:20:09 -0500

    Trailsolution update to source full driving list

--- happytrails/app/models/trailsolution.rb
+++ happytrails/app/models/trailsolution.rb
@@ -1,6 +1,6 @@
 class Trailsolution < ActiveRecord::Base
   
-  def self.getdistances(startpoint_address, transit_type, set_of_trail_head_addresses, departure_time)
+  def self.getdistances(startpoint_address, transit_type, set_of_trail_head_addresses, launch_time, time_type)
   	
   	# First, I am going to make a string for the API request. It needs to be under 2000 characters long.
 
@@ -14,22 +14,39 @@ class Trailsolution < ActiveRecord::Base
 	  		mode = "driving"
   	end
 
+    #Replace spaces with + signs
   	origin = startpoint_address.gsub(/\s/,"+")
-  	
-  	# You need to regex the address strings to have + between words
 
-  	set_of_trail_head_addresses.each do |trail|
+    #Convert times to epoch
+    time = DateTime.parse(launch_time).to_time.to_i
+
+    # figure out whether you are dealing with a start time or an end time
+    if time_type == "departs at"
+      time_target = "departure_time="
+    else
+      time_target = "arrival_time="
+    end
+
+    time_packet = time_target.to_s+time.to_s
+
+    # The addresses then need to be strung together as follows: latitude,longitude|latitude,longitude
+    destination_string = ""
+  	destination_list = []
+
+    set_of_trail_head_addresses.each do |trail|
   		latitude = trail[:latitude]
   		longitude = trail[:longitude]
-  		destination = latitude+longitude
-  	end
+  		destination = latitude.to_s+","+longitude.to_s
+      destination_list<<destination
+    end
 
+    destination_list.each{|destination| destination_string<<destination.to_s+"|"}
 
-  		packet = "origin=#{origin}&destination=#{destination}&mode=#{mode}&#{}"
+  		packet = "origin=#{origin}&#{time_packet}&mode=#{mode}&destination=#{destinationset}"
 
   	addresspackets < packet
   	
-  	response = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?#{addresspackets}")  	
+  	response = HTTParty.get("https://maps.googleapis.com/maps/api/distancematrix/json?#{addresspackets}")  	
 
  	response.to_json
   end
