2b1265695e4f931788a5355e53f6493e658f7a2c
dahvvv <davidjosephkoenig@gmail.com>
2014-11-21 11:45:54 -0500

    midway commit so ian can rename everything

--- happytrails/app/controllers/trailsolutions_controller.rb
+++ happytrails/app/controllers/trailsolutions_controller.rb
@@ -5,10 +5,15 @@ class TrailsolutionsController < ApplicationController
     city = params[:city] || ""
     state = params[:state] || ""
     zip = params[:zip] || ""
-    @startpoint_address = address + " " + city + " " + state + " " + zip
+    startpoint_address = address + " " + city + " " + state + " " + zip
+    # @trails = Trail.search_by(startpoint_address)
+    @trails = []
+    10.times do |i|
+      @trails.push(Trail.all.sample)
+    end
     respond_to do |format|
       format.html
-      format.json { render :json => {startpoint_address: @startpoint_address}}
+      format.json { render :json => {startpoint_address: startpoint_address}}
     end
   end
 
@@ -17,4 +22,3 @@ class TrailsolutionsController < ApplicationController
   end
 
 end
-
