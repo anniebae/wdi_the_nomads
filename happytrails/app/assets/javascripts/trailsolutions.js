4158cec4e328d448e3257fb35ddf999321f16ff4
dahvvv <davidjosephkoenig@gmail.com>
2014-11-21 06:51:55 -0500

    animation refactor

--- happytrails/app/assets/javascripts/trailsolutions.js
+++ happytrails/app/assets/javascripts/trailsolutions.js
@@ -1,3 +1,37 @@
+function animateTitle(title, speed){
+  $(title).animate({
+    "margin-top": "50px"
+  }, speed);
+  $(title).find($('h1')).animate({
+    "font-size": "35px"
+  }, speed);
+}
+
+function animateLocationForm(form, speed){
+  $(form).closest('.container').animate({
+    "width": "80%", "height": "7em", "margin-bottom": "20px"
+  }, speed);
+  $(form).find('.welcome-submit').animate({
+    "top":"45%","width":"10%","left":"45%"
+  }, speed);
+  $(form).find('.form-p').animate({
+    "width": "20%", "top": "-16%"
+  }, speed);
+  $(form).find('.form-p#address').animate({
+    "left": "2%"
+  }, speed);
+  $(form).find('.form-p#city').animate({
+    "left": "27%"
+  }, speed);
+  $(form).find('.form-p#state').animate({
+    "left": "52%"
+  }, speed);
+  $(form).find('.form-p#zip').animate({
+    "left": "77%"
+  }, speed);  
+}
+
+
 $(document).ready(function() {
   $(".drop-hike").hide();
   $(".drop-hello").hide();
@@ -10,40 +44,13 @@ $(document).ready(function() {
     $(".drop-hello").slideToggle();
   });
 
-  $('.welcome-submit').on('click', function(e){
+  $('.location-form').on('submit', function(e){
     e.preventDefault();
-    $('.enter-location').css({"display":"none"});
-    $('.hike-sign').animate({
-      "margin-top": "50px"
-    }, 600);
-    $('.hike-sign').find($('h1')).animate({
-      "font-size": "35px"
-    }, 600);
-    $('.container').animate({
-      "width": "80%", "height": "7em", "margin-bottom": "20px"
-    }, 600);
-    $('.hike-sign').find($('h1')).animate({
-      "font-size": "35px"
-    }, 600);
-    $('.form-p').animate({
-      "width": "20%", "top": "-16%"
-    });
-    $('#address').animate({
-      "left": "2%"
-    });
-    $('#city').animate({
-      "left": "27%"
-    });
-    $('#state').animate({
-      "left": "52%"
-    });
-    $('#zip').animate({
-      "left": "77%"
-    });
-    $(this).animate({
-      "top":"45%","width":"10%","left":"45%"
-    });
-
+    var speed = 400;
+    $(this).siblings('h2').css({"display":"none"});
+    var title = $(this).parents('.center').find('.hike-sign');
+    animateTitle(title, speed);
+    animateLocationForm(this, speed);
   });
 
 });
