31ed91f1eed776980eee5993d9f588a8561cc0b1
dahvvv <davidjosephkoenig@gmail.com>
2014-11-21 10:06:14 -0500

    trailsolutions index reconnects to js, spits back startpoint_address

--- happytrails/app/controllers/trailsolutions_controller.rb
+++ happytrails/app/controllers/trailsolutions_controller.rb
@@ -1,7 +1,15 @@
 class TrailsolutionsController < ApplicationController
 
   def index
-    binding.pry
+    address = params[:address] || ""
+    city = params[:city] || ""
+    state = params[:state] || ""
+    zip = params[:zip] || ""
+    @startpoint_address = address + " " + city + " " + state + " " + zip
+    respond_to do |format|
+      format.html
+      format.json { render :json => {startpoint_address: @startpoint_address}}
+    end
   end
 
   def show
@@ -9,3 +17,4 @@ class TrailsolutionsController < ApplicationController
   end
 
 end
+
