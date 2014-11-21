7f226839351754156d8cfaa1ef404fa918c57c9a
dahvvv <davidjosephkoenig@gmail.com>
2014-11-21 13:40:07 -0500

    trails to html

--- happytrails/app/assets/javascripts/trails.js
+++ happytrails/app/assets/javascripts/trails.js
@@ -1,3 +1,5 @@
+$trails = $(".trails")
+
 function animateTitle(title, speed){
   $(title).animate({
     "margin-top": "50px"
@@ -32,8 +34,40 @@ function animateLocationForm(form, speed){
 }
 
 function secondsToHours(seconds){
-  
+  var hours = Math.floor(seconds/3600);
+  var minutes = Math.floor((seconds%3600)/60);
+  return hours + " hrs " + minutes + " minutes away"
 }
+
+function displayTrails(trails){
+  $trails.empty();
+  $(trails).each(function(index, trail){
+    var trailHTML = trailToHTML(trail);
+    $trails.append(trailHTML);
+  });
+}
+
+function trailToHTML(trail){
+  $li = $("<li>");
+  $li.addClass("trail");
+
+  $spanTravelDist = $("<span>");
+  $spanTravelDist.addClass("travel_distance");
+  $spanTravelDist.text(secondsToHours(trail.drivingfromgrandcentralseconds));
+  $spanTitle = $("<span>");
+  $spanTitle.addClass("title");
+  $spanTitle.text(trail.title);
+  $spanTrailLength = $("<span>");
+  $spanTrailLength.addClass("trail_length");
+  $spanTrailLength.text(trail.length);
+
+  $li.append($spanTravelDist);
+  $li.append($spanTitle);
+  $li.append($spanTrailLength);
+
+  return $li;
+}
+
 
 
 $(document).ready(function() {
@@ -73,13 +107,10 @@ $(document).ready(function() {
       },
       success: function(data){
         var trails = data.trails;
-        debugger;
-        $.each(trails, function(index, value){
-          value.drivingfromgrandcentralseconds
-        })
-        console.log(startPointAddress)
+        displayTrails(trails);
       }
     });
   });
 
 });
+
